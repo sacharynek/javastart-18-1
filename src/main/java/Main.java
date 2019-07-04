c1292b5e60be1294068a1f0ae75d2c3627c49c98
tomaszlach <tomaszlach86@gmail.com>
2019-07-04 18:36:44 +0200

    final polish

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -10,26 +10,8 @@ public class Main {
      */
     public static void main(String[] args) {
 
-        System.out.println(calculateFibElements(4_000_000));
+        System.out.println(Fibonacci.calculateFibElements(10));
 
-    }
-
-    public static long calculateFibElements(int last) {
-        long a = 0, b = 1, c = 0, sum = 0;
-
-        for (int i = 1; i < last; i++) {
-            c = a + b;
-            a = b;
-            b = c;
-            System.out.println("element numer " + i + " ciagu fibunacciego to " + c);
-            if (c >= 4_000_000) {
-                break;
-            }
-            if (c % 2 == 0) {
-                sum += c;
-            }
-        }
-        return sum;
     }
 
 }
---
+++ src/test/java/FibonacciTest.java
@@ -0,0 +1,23 @@
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class FibonacciTest {
+
+    @DataProvider
+    public Object[][] getData() {
+        return new Object[][]{{4_000_000, 4613732}, {10, 44}, {0, 0}, {-1, 0}};
+    }
+
+    @Test(dataProvider = "getData")
+    public void testCalculateFibElements(int input, int output) {
+
+        //Arrange
+        //Act
+        long calculatedValue = Fibonacci.calculateFibElements(input);
+        //Assert
+        assertThat(calculatedValue).isEqualTo(output);
+    }
+
+}
